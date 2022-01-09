fd3f9bf1bdb44ddeec48bf3b299a9f3790a408bb
jesica <jesicaguerrerof@gmail.com>
2022-01-09 19:01:51 -0300

    detalle de pelicula ok

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -12,4 +12,14 @@ module.exports={
         })
         .catch(error => console.log(error))
     },
+    detail : (req,res) => {
+        db.Movie.findByPk(req.params.id)
+            .then(movies => {
+                /* res.send(movie) */
+                return res.render('moviesDetail',{
+                    movies
+                })
+            })
+            .catch(error => console.log(error))
+    },
 }
